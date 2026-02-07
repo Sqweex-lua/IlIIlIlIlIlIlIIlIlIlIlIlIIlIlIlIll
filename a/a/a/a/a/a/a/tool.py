bf84abc9fb9b33a084d476e4f8db293ceed2eaa4
Sqweex-lua <romavinokurov300@gmail.com>
2026-02-07 18:36:27 +0300

    Update tool.py

--- a/a/a/a/a/a/a/tool.py
+++ a/a/a/a/a/a/a/tool.py
@@ -5,8 +5,8 @@ from telethon.sync import TelegramClient
 from telethon import functions, types, errors
 
 # cfgg
-API_ID = 26152851
-API_HASH = '160aceff0bc4565f0ca9f5d1e24e17a1'
+API_ID = 22715257
+API_HASH = '7982af310274ecf28b9e5e3475e7eaa3 '
 SESSIONS_DIR = 'sessions'
 DELAY = 2
 
